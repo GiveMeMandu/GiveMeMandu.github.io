3db5f344d1fbeb31a508ee41c4ba9b6d3841de8a
GiveMeMandu <lion0303@hanyang.ac.kr>
2020-10-01 14:25:14 +0000

    Updating to GiveMeMandu/GiveMeMandu.github.io@17dae01cef172a2b429957232c2158523eb12fc3.

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "minimal mistakes 테마를 이용해 github.io 블로그 구축하기",
-        "excerpt":" ","categories": ["blogging"],
-        "tags": [],
-        "url": "https://givememandu.github.io//blogging/first-post/",
+        "excerpt":"Jekyll 사용.   댓글은 Disqus   빌드는 Travis-CI   YFM에서 정의한 제목을 이중 괄호 구문으로 본문에 추가 가능. 이 글의 제목은 minimal mistakes 테마를 이용해 github.io 블로그 구축하기이고 마지막으로 수정된 시간은 2020-10-01 13:11:30 +0900이다.   푸쉬전 로컬에서 최종확인 때 쓰는 명령어는   #기본 디렉토리에서  bundle exec jekyll serve  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "https://givememandu.github.io//blog/first-post/",
         "teaser": null
       }]
